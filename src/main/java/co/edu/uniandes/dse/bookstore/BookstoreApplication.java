fbd69a5a068ffe0e5b144978b908a99b1bb126db
Jose Bocanegra <j.bocanegra@uniandes.edu.co>
2021-06-29 11:01:58 -0500

    Fix JUnit version

--- src/main/java/co/edu/uniandes/dse/bookstore/BookstoreApplication.java
+++ src/main/java/co/edu/uniandes/dse/bookstore/BookstoreApplication.java
@@ -1,18 +1,11 @@
 package co.edu.uniandes.dse.bookstore;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookstoreApplication {
 	
-	@Bean
-	public ModelMapper modelMapper() {
-	    return new ModelMapper();
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
---
+++ src/main/java/co/edu/uniandes/dse/bookstore/config/ApplicationConfig.java
@@ -0,0 +1,15 @@
+package co.edu.uniandes.dse.bookstore.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+
+   @Bean
+   public ModelMapper modelMapper() {
+      ModelMapper modelMapper = new ModelMapper();
+      return modelMapper;
+   }
+}
