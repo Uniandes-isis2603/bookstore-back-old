f5417578b2cfae41048abc7bccc05a97de7ce0ec
Jose Bocanegra <j.bocanegra@uniandes.edu.co>
2021-07-01 07:29:41 -0500

    Fix JUnit version

--- src/test/java/co/edu/uniandes/dse/bookstore/BookstoreApplicationTests.java
+++ src/test/java/co/edu/uniandes/dse/bookstore/BookstoreApplicationTests.java
@@ -1,5 +1,7 @@
 package co.edu.uniandes.dse.bookstore;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class BookstoreApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
