3dd547fdc6d8cb9cfc0b526ea89aa74012c88c4a
Jose Bocanegra <jose@verne.me>
2022-02-14 18:56:20 -0500

    Fix base entity anotation

--- src/main/java/co/edu/uniandes/dse/bookstore/entities/BaseEntity.java
+++ src/main/java/co/edu/uniandes/dse/bookstore/entities/BaseEntity.java
@@ -24,8 +24,6 @@ SOFTWARE.
 
 package co.edu.uniandes.dse.bookstore.entities;
 
-import java.util.Objects;
-
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -49,27 +47,4 @@ public abstract class BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	@Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final BaseEntity other = (BaseEntity) obj;
-        return Objects.equals(this.id, other.id);
-    }
-
-    @Override
-    public int hashCode() {
-        if (this.getId() != null) {
-            return this.getId().hashCode();
-        }
-        return super.hashCode();
-    }
 }
