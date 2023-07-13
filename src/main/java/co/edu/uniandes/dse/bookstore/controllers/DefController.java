dcbcb52beab176630f10ae62eefe8125f881ec61
José Bocanegra <j.bocanegra@uniandes.edu.co>
2023-07-13 16:38:46 -0500

    Update and rename DefaultController.java to DefController.java

--- src/main/java/co/edu/uniandes/dse/bookstore/controllers/DefController.java
+++ src/main/java/co/edu/uniandes/dse/bookstore/controllers/DefController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("*")
-public class DefaultController {
+public class DefController {
     @GetMapping
     @ResponseStatus(code = HttpStatus.OK)
     public Map<String, String> welcome() {
